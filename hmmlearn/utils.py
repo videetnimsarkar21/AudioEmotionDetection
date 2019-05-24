6d12176cd0a9c95b684c0f93183ef9e18b046b82
Videet Nimsarkar <39655523+Videet21@users.noreply.github.com>
2019-05-25 02:14:49 +0530

    Add files via upload

---
+++ hmmlearn/utils.py
@@ -0,0 +1,148 @@
+import numpy as np
+from scipy.misc import logsumexp
+
+
+def normalize(a, axis=None):
+    """Normalizes the input array so that it sums to 1.
+
+    Parameters
+    ----------
+    a : array
+        Non-normalized input data.
+
+    axis : int
+        Dimension along which normalization is performed.
+
+    Notes
+    -----
+    Modifies the input **inplace**.
+    """
+    a_sum = a.sum(axis)
+    if axis and a.ndim > 1:
+        # Make sure we don't divide by zero.
+        a_sum[a_sum == 0] = 1
+        shape = list(a.shape)
+        shape[axis] = 1
+        a_sum.shape = shape
+
+    a /= a_sum
+
+
+def log_normalize(a, axis=None):
+    """Normalizes the input array so that the exponent of the sum is 1.
+
+    Parameters
+    ----------
+    a : array
+        Non-normalized input data.
+
+    axis : int
+        Dimension along which normalization is performed.
+
+    Notes
+    -----
+    Modifies the input **inplace**.
+    """
+    a_lse = logsumexp(a, axis)
+    a -= a_lse[:, np.newaxis]
+
+
+def iter_from_X_lengths(X, lengths):
+    if lengths is None:
+        yield 0, len(X)
+    else:
+        n_samples = X.shape[0]
+        end = np.cumsum(lengths).astype(np.int32)
+        start = end - lengths
+        if end[-1] > n_samples:
+            raise ValueError("more than {0:d} samples in lengths array {1!s}"
+                             .format(n_samples, lengths))
+
+        for i in range(len(lengths)):
+            yield start[i], end[i]
+
+
+def log_mask_zero(a):
+    """Computes the log of input probabilities masking divide by zero in log.
+
+    Notes
+    -----
+    During the M-step of EM-algorithm, very small intermediate start
+    or transition probabilities could be normalized to zero, causing a
+    *RuntimeWarning: divide by zero encountered in log*.
+
+    This function masks this unharmful warning.
+    """
+    a = np.asarray(a)
+    with np.errstate(divide="ignore"):
+        a_log = np.log(a)
+        a_log[a <= 0] = 0.0
+        return a_log
+
+
+def fill_covars(covars, covariance_type='full', n_components=1, n_features=1):
+    if covariance_type == 'full':
+        return covars
+    elif covariance_type == 'diag':
+        return np.array(list(map(np.diag, covars)))
+    elif covariance_type == 'tied':
+        return np.tile(covars, (n_components, 1, 1))
+    elif covariance_type == 'spherical':
+        eye = np.eye(n_features)[np.newaxis, :, :]
+        covars = covars[:, np.newaxis, np.newaxis]
+        return eye * covars
+
+
+def distribute_covar_matrix_to_match_covariance_type(
+        tied_cv, covariance_type, n_components):
+    
+    if covariance_type == 'spherical':
+        cv = np.tile(tied_cv.mean() * np.ones(tied_cv.shape[1]),
+                     (n_components, 1))
+    elif covariance_type == 'tied':
+        cv = tied_cv
+    elif covariance_type == 'diag':
+        cv = np.tile(np.diag(tied_cv), (n_components, 1))
+    elif covariance_type == 'full':
+        cv = np.tile(tied_cv, (n_components, 1, 1))
+    else:
+        raise ValueError("covariance_type must be one of " +
+                         "'spherical', 'tied', 'diag', 'full'")
+    return cv
+
+def _validate_covars(covars, covariance_type, n_components):
+    """Do basic checks on matrix covariance sizes and values."""
+    from scipy import linalg
+    if covariance_type == 'spherical':
+        if len(covars) != n_components:
+            raise ValueError("'spherical' covars have length n_components")
+        elif np.any(covars <= 0):
+            raise ValueError("'spherical' covars must be non-negative")
+    elif covariance_type == 'tied':
+        if covars.shape[0] != covars.shape[1]:
+            raise ValueError("'tied' covars must have shape (n_dim, n_dim)")
+        elif (not np.allclose(covars, covars.T)
+              or np.any(linalg.eigvalsh(covars) <= 0)):
+            raise ValueError("'tied' covars must be symmetric, "
+                             "positive-definite")
+    elif covariance_type == 'diag':
+        if len(covars.shape) != 2:
+            raise ValueError("'diag' covars must have shape "
+                             "(n_components, n_dim)")
+        elif np.any(covars <= 0):
+            raise ValueError("'diag' covars must be non-negative")
+    elif covariance_type == 'full':
+        if len(covars.shape) != 3:
+            raise ValueError("'full' covars must have shape "
+                             "(n_components, n_dim, n_dim)")
+        elif covars.shape[1] != covars.shape[2]:
+            raise ValueError("'full' covars must have shape "
+                             "(n_components, n_dim, n_dim)")
+        for n, cv in enumerate(covars):
+            if (not np.allclose(cv, cv.T)
+                    or np.any(linalg.eigvalsh(cv) <= 0)):
+                raise ValueError("component %d of 'full' covars must be "
+                                 "symmetric, positive-definite" % n)
+    else:
+        raise ValueError("covariance_type must be one of " +
+                         "'spherical', 'tied', 'diag', 'full'")
